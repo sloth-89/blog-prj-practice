8d0f8189eeb83ce22789e4ffef1d9b82f5e08e92
restvoice2020@gmail.com <restvoice2020@gmail.com>
2023-05-24 17:35:48 +0900

    blog prj 01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+/* eslint-disable */ 
+
 import './App.css';
+import { useState } from 'react';
+import Modal from './component/Modal';
 
 function App() {
+
+  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [num, setNum] = useState(0);
+
+  let handleClick = () => {
+    setNum(num + 1);
+  }
+
+  let handleChange = () => {
+    let newArray = [...title];
+    newArray[0] = '여자 코트 추천';
+    setTitle(newArray);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='black-nav'>
+        <div>개발 Blog</div>
+        <nav></nav>
       </header>
+      <button onClick={handleChange}>변경</button>
+      <div className='list'>
+        <h3>{title[0]} <span onClick={handleClick}>👍</span> {num} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3>{title[1]}</h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3>{title[2]}</h3>
+        <p>2월 19일 발행</p>
+        <hr/>
+      </div>
+
+      <Modal/>
+
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/component/Modal.js
@@ -0,0 +1,10 @@
+
+export default function Modal(){
+    return(
+      <div className='modal'>
+          <h2>제목</h2>
+          <p>날씨</p>
+          <p>상세내용</p>
+      </div>
+    )
+  }
